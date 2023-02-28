bfd231a7e20eb4e6ab394bf2779f7b500c99e3de
Iyan Theekshana <iyan.dev123@gmail.com>
2023-02-28 12:18:59 +0100

    implemented special attack, heal, surrender, new game buttons

--- app.js
+++ app.js
@@ -7,27 +7,91 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
+      currentRound: 0,
+      winner: null,
     };
   },
   methods: {
     attackMonster() {
       const attactValue = getRandomValue(5, 12);
       this.monsterHealth -= attactValue;
-      that = this;
-
-      that.attackPlayer();
+      this.attackPlayer();
+      this.currentRound++;
     },
     attackPlayer() {
       const attactValue = getRandomValue(8, 15);
       this.playerHealth -= attactValue;
     },
+    specialAttactMonster() {
+      const attactValue = getRandomValue(10, 25);
+      this.monsterHealth -= attactValue;
+      this.attackPlayer();
+      this.currentRound++;
+    },
+    mayUserSpeacialAttcak() {
+      return this.currentRound % 3 !== 0 || this.currentRound === 0;
+    },
+    healPlayer() {
+      const healValue = getRandomValue(10, 25);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+        this.attackPlayer();
+        this.currentRound++;
+      }
+    },
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+    },
+    surrender() {
+      this.winner = "You lost";
+    },
+    maySurrender() {
+      return (
+        this.playerHealth === 100 &&
+        this.monsterHealth === 100 &&
+        this.currentRound === 0
+      );
+    },
   },
   computed: {
     monsterBarStyle() {
-      return { width: this.monsterHealth + "%" };
+      if (this.monsterHealth < 0) {
+        return {
+          width: "0%",
+        };
+      } else {
+        return { width: this.monsterHealth + "%" };
+      }
     },
     playerBarStyle() {
-      return { width: this.playerHealth + "%" };
+      if (this.playerHealth < 0) {
+        return {
+          width: "0%",
+        };
+      } else {
+        return { width: this.playerHealth + "%" };
+      }
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        this.winner = "Draw";
+      } else if (value <= 0) {
+        this.winner = "You lost";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        this.winner = "Draw";
+      } else if (value <= 0) {
+        this.winner = "You win";
+      }
     },
   },
 });
